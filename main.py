34a80414a7616b9e6c1b99461b5b7a22f3693d69
Son Do <dominhson580@gmail.com>
2023-04-08 16:07:44 +0700

    Count each letter frequency

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+import string
+
 fpath = 'books/frankenstein.txt'
 
 with open(fpath) as fhandle:
     content = fhandle.read()
 
-print(content)
+# print(content)
+
+words = content.split()
+words_count = len(words)
+
+letters_count = {}
+for letter in content:
+    if letter not in string.ascii_letters:
+        continue
+    letter = letter.lower()
+    letters_count[letter] = letters_count.get(letter, 0) + 1
